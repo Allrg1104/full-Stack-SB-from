b56b6a7ee2ae867ad80538784b489b24821bb822
Rivera G Alejandro <alejandro110498@hotmail.com>
2025-05-09 20:39:53 -0500

    Update V2 ChatPrompt.jsx

--- src/components/ChatPrompt.jsx
+++ src/components/ChatPrompt.jsx
@@ -23,8 +23,8 @@ const ChatPrompt = () => {
   const obtenerEdificios = async () => {
     try {
       setCargandoEdificios(true)
-      //const response = await axios.get("https://edificios-back.vercel.app/api/chat/edificios")
-      const response = await axios.get("http://localhost:5000/api/chat/edificios")
+      const response = await axios.get("https://edificios-back.vercel.app/api/chat/edificios")
+      //const response = await axios.get("http://localhost:5000/api/chat/edificios")
       console.log("Datos de aulas:", response.data);
       console.log("Edificio seleccionado:", edificioSeleccionado);
       console.log("Claves de salonesPorEdificio:", Object.keys(salonesPorEdificio));
@@ -46,8 +46,8 @@ const ChatPrompt = () => {
   const obtenerSalonesPorEdificio = async (edificio) => {
     try {
       setCargandoSalones(true)
-      //const response = await axios.get(`https://edificios-back.vercel.app/api/aulas/edificio/${edificio}`)
-      const response = await axios.get(`http://localhost:5000/api/chat/aulas/edificio/${edificio}`)
+      const response = await axios.get(`https://edificios-back.vercel.app/api/aulas/edificio/${edificio}`)
+      //const response = await axios.get(`http://localhost:5000/api/chat/aulas/edificio/${edificio}`)
       // Crear un objeto con el formato esperado
       const salones = response.data.map((aula) => ({
         nombre: aula.nombre_salon,
